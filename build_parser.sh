6c6172cc772c3cd135e5eedd95f3a66f70d3d1a6
John Rothfield <rthfield@sonic.net>
2011-11-25 16:40:32 -0800

    renamed sargam_parser to doremi_script_parser

--- build_parser.sh
+++ build_parser.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 # Shell script to compile the sargam peg grammar
 # Puts compiled grammar into both lib and the web app public directory
-GRAMMAR=sargam.peg.js
-LIB=./lib/sargam/sargam_parser.js
-WEBAPP=./webapp/doremi_web/public/js/sargam_parser.js
+GRAMMAR=doremiscript.peg.js
+LIB=./lib/doremi-script/doremi_script_parser.js
+WEBAPP=./webapp/doremi_web/public/js/doremi_script_parser.js
 echo "Running pegjs on $GRAMMAR to create parser in $LIB"
 pegjs -e SargamParser ./src/grammars/$GRAMMAR $LIB 
 echo "copying to $WEBAPP"
