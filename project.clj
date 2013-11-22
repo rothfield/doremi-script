14220e455af0812a25fa0021fffa025aad72c1f4
John Rothfield <rthfield@sonic.net>
2013-11-21 21:49:18 -0800

    cli

--- project.clj
+++ project.clj
@@ -3,14 +3,10 @@
   :url "http://github.com/rothfield/doremi-script-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-localrepo "0.5.2"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.3"]
                  [instaparse "1.2.2"] 
                  [com.googlecode.texhyphj/texhyphj "1.2"]
-                ;; [com.stuartsierra/clojure.walk2 "0.1.0-SNAPSHOT"]
-                ;; [net.davidashen/texhypj "1.0"]
-               ;;  [net.davidashen.text.Hyphenator "1.0"]
                  ]
   :main doremi_script_clojure.core
   :profiles {:uberjar {:aot :all}}
--- src/doremi_script_clojure/core.clj
+++ src/doremi_script_clojure/core.clj
@@ -2,9 +2,11 @@
   (:gen-class)
   (:require	
     [instaparse.core :as insta]
+
     [doremi_script_clojure.semantic_analyzer :refer [transform-parse-tree]]
     [clojure.java.io :refer [input-stream resource]]
     [clojure.data.json :as json]
+    [clojure.pprint :refer [pprint]] 
     ))
 
 
@@ -27,9 +29,29 @@
 (defn run-through-parser[txt]
   (doremi-script-parser txt))
 
+
+(defn get-stdin[]
+  (with-open [rdr (java.io.BufferedReader. *in* )]
+      (let [seq  (line-seq rdr)
+            zz (count seq)]
+            (apply str (clojure.string/join "\n" seq)))))
+  
+(defn -mzzain
+  [& args]
+  (get-stdin))
+;;  (println (get-stdin)))
+
+
 (defn -main[& args]
-  (let [txt (str (slurp *in*) "\n")
-       x (transform-parse-tree (run-through-parser txt) txt)
+  (let [
+       ;; txt1 (slurp (first args))
+        txt (get-stdin)
+
+       ;;  _ (println "txt is" txt)
+       x (transform-parse-tree (run-through-parser  txt)
+                                txt)
         ]
-  (json/pprint x :key-fn json-key-fn)))
+  (json/pprint x :key-fn json-key-fn)
+    (println "")
+   ""))
 
