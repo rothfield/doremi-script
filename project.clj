0b9070c97b270dc2dbb2af7f0f49f5d97bafee98
John Rothfield <rthfield@sonic.net>
2014-03-04 14:01:45 -0800

    INITIAL integration of Clojurescript. Updated project to include clojurescript. Add directory for clojure source files (src-cljs). Created directory for compiled clojurescript files (resources/public/cljs_js). Got hello world example to work

--- project.clj
+++ project.clj
@@ -5,17 +5,19 @@
                  [org.clojure/clojure "1.5.1"]
                  [hiccup "1.0.5"]
                  [compojure "1.1.6"]
-                [ring/ring-json "0.2.0"]
+                 [ring/ring-json "0.2.0"]
                  [ring/ring-jetty-adapter "1.2.0"] 
                  [org.clojars.mikejs/ring-etag-middleware "0.1.0-SNAPSHOT"]
                  [instaparse "1.2.2"] 
                  [ring.middleware.logger "0.4.0"]
+                 [org.clojure/clojurescript "0.0-2173"]
                  ]
-                ;;  [javax.servlet/servlet-api "2.5"]
-                 ;;                 [com.googlecode.texhyphj/texhyphj "1.2"]
-  :plugins [[lein-ring "0.8.10"]]
+  ;;                 [com.googlecode.texhyphj/texhyphj "1.2"]
+  :plugins [[lein-ring "0.8.10"]
+            [lein-cljsbuild "1.0.2"]
+            ]
   ;; You can run the standalone jar as follows
-  ;; java -jar target/doremi-script-standalone.jar
+  ;; java -jar doremi.jar -s  to run server
   ;; It will run the main function in doremi_core
   :main doremi_script.doremi_main 
   :jar-name "doremi-script.jar"
@@ -24,4 +26,27 @@
   :profiles { :uberjar {:aot :all}
              :dev {:dependencies [
                                   [javax.servlet/servlet-api "2.5"]
-                                  [ring-mock "0.1.5"]]}})
+                                  [ring-mock "0.1.5"]]}}
+
+  :cljsbuild {
+              :builds {
+                       :dev {
+                             :source-paths ["src-cljs"]
+                             :compiler {:output-to "resources/public/cljs_js/doremi_cljs.js"
+                                        :output-dir "resources/public/cljs_js"
+                                        :optimizations :none
+                                        :pretty-print true
+                                        :source-map "resources/public/cljs/doremi_cljs.js.map"}}
+
+;;                     :prod {
+;;                            :source-paths ["src-cljs"]
+;;                            :compiler {:output-to "resources/public/js-min/doremi_cljs_min.js"
+;;                                       :output-dir "resources/public/js-min"
+;;                                       :optimizations :advanced
+;;                                       :pretty-print false
+;;                                       :source-map "resources/public/js-min/doremi_cljs_min.js.map"}}
+                       }
+              }
+
+
+  )
---
+++ resources/public/cljs_js/doremi_script/core.cljs
@@ -0,0 +1,22 @@
+(ns doremi_script.core
+  (:require [doremi_script.foo.bar :as bar]))
+
+(defn ^{:export greet} greet [n]
+  (str "Hello " n))
+
+(defn ^:export sum [xs]
+  (bar/sum xs))
+
+(defn ^:export let-works? [day month year]
+  (let [hour (first day)
+        minutes (first hour)
+        seconds (last hour)]
+    (.log js/console "Date: " year month day)
+    (str year month day hour minutes seconds)))
+
+(defn bailey [proton neutron electron & comedies]
+  (apply + proton neutron electron)
+  (map identity comedies))
+
+(defn videotape [& rainbows]
+  (map :params rainbows))
